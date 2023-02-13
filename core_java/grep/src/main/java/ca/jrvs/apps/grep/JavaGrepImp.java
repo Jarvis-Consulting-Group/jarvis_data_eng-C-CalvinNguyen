cfb63178c26939029eb20cf7a2aea093c5544343
Chi Calvin Nguyen <chicalvin.nguyen1@gmail.com>
2023-02-13 08:05:47 +0000

    reformated code and optimized imports

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrepImp.java
@@ -1,15 +1,12 @@
 package ca.jrvs.apps.grep;
 
-import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
