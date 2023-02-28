031e2e0da6741783ff28103d655070f17ad9b21e
Chi Calvin Nguyen <chicalvin.nguyen1@gmail.com>
2023-02-28 03:39:10 +0000

    Completed HttpHelper, to do implement TwitterDao

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/Controller.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/controller/Controller.java
@@ -2,10 +2,13 @@ package ca.jrvs.apps.twitter.controller;
 
 public interface Controller {
 
+  /*
   Tweet postTweet(String[] args);
 
   Tweet showTweet(String[] args);
 
   List<Tweet> deleteTweet(String[] args);
+
+   */
 
 }
--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/dao/helper/HttpHelper.java
@@ -7,6 +7,11 @@ public interface HttpHelper {
 
   HttpResponse httpPost(URI uri);
 
+  HttpResponse httpPostV2(URI uri, String s);
+
   HttpResponse httpGet(URI uri);
 
+  HttpResponse httpDelete(URI uri);
+
+  HttpResponse httpDeleteV2(URI uri);
 }
