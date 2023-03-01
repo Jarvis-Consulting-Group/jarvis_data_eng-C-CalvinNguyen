f7c4ea32714b9a2579d1c1e4680865b45837c761
Chi Calvin Nguyen <chicalvin.nguyen1@gmail.com>
2023-03-01 21:52:58 +0000

    Working on testing service implementation

--- core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/Service.java
+++ core_java/twitter/src/main/java/ca/jrvs/apps/twitter/service/Service.java
@@ -1,14 +1,40 @@
 package ca.jrvs.apps.twitter.service;
 
-public interface Service {
+import java.util.List;
 
-  /*
-  Tweet postTweet(Tweet tweet);
+/**
+ * Service to be implemented that contains the business logic, it will use the TwitterDao(V2) to
+ * perform its actions. It also verifies if the inputs are acceptable, and if not throws exceptions.
+ * @param <T> Generic type, can be implemented as Tweet or TweetV2.
+ */
+public interface Service<T> {
 
-  Tweet showTweet(String id, String[] fields);
-
-  List<Tweet> deleteTweets(String[] ids);
-
+  /**
+   * Validate the user's input and then post the tweet.
+   * @param tweet object to be created/posted.
+   * @return returns the created tweet from API.
+   *
+   * @throws IllegalArgumentException it will throw an exception if the number of characters exceeds
+   * the maximum allowed.
    */
+  T postTweet(T tweet);
 
+  /**
+   * Search/Find a tweet by the tweets ID.
+   * @param id the ID of the tweet.
+   * @param fields tweet fields to be returned.
+   * @return TweetV2 object populated with information from Twitter API
+   *
+   * @throws IllegalArgumentException throws an exception if ID or Fields is invalid.
+   */
+  T showTweet(String id, String[] fields);
+
+  /**
+   * Delete one or more tweets by the id(s) in the array.
+   * @param ids array of tweet IDs to be deleted.
+   * @return return a list of tweets that were deleted.
+   *
+   * @throws IllegalArgumentException throws an exception if one of the IDs is invalid.
+   */
+  List<T> deleteTweets(String[] ids);
 }
