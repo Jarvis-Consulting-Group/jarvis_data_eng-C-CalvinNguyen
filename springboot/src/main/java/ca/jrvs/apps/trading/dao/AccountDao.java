065cd09627e31571118340380b63e61c88703d8d
Chi Calvin Nguyen <chicalvin.nguyen1@gmail.com>
2023-03-27 19:09:52 +0000

    completed accountdao and integration testing

--- springboot/src/main/java/ca/jrvs/apps/trading/dao/AccountDao.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/AccountDao.java
@@ -1,6 +1,6 @@
 package ca.jrvs.apps.trading.dao;
 
-import ca.jrvs.apps.trading.model.domain.Trader;
+import ca.jrvs.apps.trading.model.domain.Account;
 import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,16 +10,16 @@ import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public class TraderDao extends JdbcCrudDao<Trader> {
+public class AccountDao extends JdbcCrudDao<Account> {
 
-  private static final Logger logger = LoggerFactory.getLogger(TraderDao.class);
-  private static final String TABLE_NAME = "trader";
+  private static final Logger logger = LoggerFactory.getLogger(AccountDao.class);
+  private static final String TABLE_NAME = "account";
   private static final String ID_COLUMN = "id";
   private JdbcTemplate jdbcTemplate;
   private SimpleJdbcInsert simpleJdbcInsert;
 
   @Autowired
-  public TraderDao(DataSource dataSource) {
+  public AccountDao(DataSource dataSource) {
     this.jdbcTemplate = new JdbcTemplate(dataSource);
     this.simpleJdbcInsert = new SimpleJdbcInsert(dataSource)
         .withTableName(TABLE_NAME)
@@ -47,22 +47,30 @@ public class TraderDao extends JdbcCrudDao<Trader> {
   }
 
   @Override
-  Class<Trader> getEntityClass() {
-    return Trader.class;
+  Class<Account> getEntityClass() {
+    return Account.class;
   }
 
   @Override
-  public int updateOne(Trader entity) {
+  public int updateOne(Account entity) {
+    String updateSql = "UPDATE " + getTableName()
+        + " SET amount=? WHERE " + getIdColumnName() + "=?";
+
+    Object[] updatedValues = {
+        entity.getAmount(),
+        entity.getId()
+    };
+
+    return getJdbcTemplate().update(updateSql, updatedValues);
+  }
+
+  @Override
+  public void delete(Account account) {
     throw new UnsupportedOperationException("Not implemented");
   }
 
   @Override
-  public void deleteAll(Iterable<? extends Trader> iterable) {
-    throw new UnsupportedOperationException("Not implemented");
-  }
-
-  @Override
-  public void delete(Trader trader) {
+  public void deleteAll(Iterable<? extends Account> iterable) {
     throw new UnsupportedOperationException("Not implemented");
   }
 }
