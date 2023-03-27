74a78002a165752b71c20b252bbf4c8d64074e66
Chi Calvin Nguyen <chicalvin.nguyen1@gmail.com>
2023-03-27 18:27:35 +0000

    completed jdbccruddao, traderdao and trader integration testing

--- springboot/src/main/java/ca/jrvs/apps/trading/dao/JdbcCrudDao.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/dao/JdbcCrudDao.java
@@ -1,11 +1,14 @@
 package ca.jrvs.apps.trading.dao;
 
 import ca.jrvs.apps.trading.model.domain.Entity;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Optional;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataRetrievalFailureException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.dao.IncorrectResultSizeDataAccessException;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
@@ -14,13 +17,22 @@ import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
+/**
+ * Temporary Message.
+ * @param <T> temp.
+ */
 public abstract class JdbcCrudDao<T extends Entity<Integer>> implements CrudRepository<T, Integer> {
 
   private static final Logger logger = LoggerFactory.getLogger(JdbcCrudDao.class);
-  abstract public JdbcTemplate getJdbcTemplate();
-  abstract public SimpleJdbcInsert getSimpleJdbcInsert();
-  abstract public String getTableName();
-  abstract public String getIdColumnName();
+
+  public abstract JdbcTemplate getJdbcTemplate();
+
+  public abstract SimpleJdbcInsert getSimpleJdbcInsert();
+
+  public abstract String getTableName();
+
+  public abstract String getIdColumnName();
+
   abstract Class<T> getEntityClass();
 
   @Override
@@ -32,6 +44,7 @@ public abstract class JdbcCrudDao<T extends Entity<Integer>> implements CrudRepo
     } else {
       addOne(entity);
     }
+
     return entity;
   }
 
@@ -42,50 +55,111 @@ public abstract class JdbcCrudDao<T extends Entity<Integer>> implements CrudRepo
     entity.setId(newId.intValue());
   }
 
-  abstract public int updateOne(T entity);
+  public abstract int updateOne(T entity);
+
+  @Override
+  public <S extends T> List<S> saveAll(Iterable<S> iterable) {
+    Iterator<S> iterator = iterable.iterator();
+    List<S> entityList = new ArrayList<>();
+
+    while (iterator.hasNext()) {
+      entityList.add(save(iterator.next()));
+    }
+
+    return entityList;
+  }
 
   @Override
   public Optional<T> findById(Integer id) {
     Optional<T> entity = Optional.empty();
-    String selectSql = "SELECT * FROM " + getTableName() + " WHERE " + getIdColumnName() + "=?";
+    String selectSql = "SELECT * FROM " + getTableName()
+        + " WHERE " + getIdColumnName() + "=?";
 
     try {
       entity = Optional.ofNullable((T) getJdbcTemplate()
-          .queryForObject(selectSql,
-              BeanPropertyRowMapper.newInstance(getEntityClass()), id));
+          .queryForObject(
+              selectSql,
+              BeanPropertyRowMapper.newInstance(getEntityClass()),
+              id
+          ));
     } catch (IncorrectResultSizeDataAccessException e) {
-      logger.debug("Can't find id: " + id, e);
+      logger.debug("Can't find with id: " + id, e);
     }
+
     return entity;
   }
 
   @Override
   public boolean existsById(Integer id) {
-    return false;
+    String existSql = "SELECT count(*) FROM " + getTableName()
+        + " WHERE " + getIdColumnName() + "=?";
+
+    Integer count = getJdbcTemplate().queryForObject(existSql, Integer.class, id);
+
+    if (count == null) {
+      return false;
+    } else {
+      return count == 1;
+    }
   }
 
   @Override
   public List<T> findAll() {
-    return null;
+    String findAllSql = "SELECT * FROM " + getTableName();
+    List<T> entityList = new ArrayList<>();
+
+    try {
+      entityList = getJdbcTemplate().query(
+          findAllSql,
+          BeanPropertyRowMapper.newInstance(getEntityClass())
+      );
+    } catch (IncorrectResultSizeDataAccessException e) {
+      logger.debug("error finding all");
+    }
+
+    return entityList;
   }
 
   @Override
   public List<T> findAllById(Iterable<Integer> ids) {
-    return null;
-  }
+    List<T> entityList = new ArrayList<>();
 
-  @Override
-  public void deleteById(Integer id) {
+    for (Integer id : ids) {
+      Optional<T> tempEntity = findById(id);
+      tempEntity.ifPresent(entityList::add);
+    }
 
+    return entityList;
   }
 
   @Override
   public long count() {
-    return 0;
+    String countSql = "SELECT count(*) FROM " + getTableName();
+
+    Long count = getJdbcTemplate().queryForObject(countSql, Long.class);
+
+    if (count == null) {
+      throw new DataRetrievalFailureException("Error retrieving count");
+    } else {
+      return count;
+    }
+  }
+
+  @Override
+  public void deleteById(Integer id) {
+    String deleteSql = "DELETE FROM " + getTableName()
+        + " WHERE " + getIdColumnName() + "=?";
+
+    int rowNum = getJdbcTemplate().update(deleteSql, id);
+
+    if (rowNum != 1) {
+      throw new DataRetrievalFailureException("");
+    }
   }
 
   @Override
   public void deleteAll() {
-
+    String deleteSql = "DELETE FROM " + getTableName();
+    getJdbcTemplate().update(deleteSql);
   }
 }
---
+++ springboot/src/main/java/ca/jrvs/apps/trading/model/domain/Trader.java
@@ -0,0 +1,64 @@
+package ca.jrvs.apps.trading.model.domain;
+
+import java.util.Date;
+
+public class Trader implements Entity<Integer> {
+
+  private Integer id;
+  private String firstName;
+  private String lastName;
+  private String email;
+  private String country;
+  private Date dob;
+
+
+  @Override
+  public Integer getId() {
+    return this.id;
+  }
+
+  @Override
+  public void setId(Integer integer) {
+    this.id = integer;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getCountry() {
+    return country;
+  }
+
+  public void setCountry(String country) {
+    this.country = country;
+  }
+
+  public Date getDob() {
+    return dob;
+  }
+
+  public void setDob(Date dob) {
+    this.dob = dob;
+  }
+}
