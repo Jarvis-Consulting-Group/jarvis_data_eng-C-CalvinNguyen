302d94510ae7eb6e5aefcca01def32b719846741
Chi Calvin Nguyen <chicalvin.nguyen1@gmail.com>
2023-03-29 17:48:37 +0000

    Completed dashboardservice and controller, to do testing

--- springboot/src/main/java/ca/jrvs/apps/trading/service/DashboardService.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/service/DashboardService.java
@@ -6,10 +6,18 @@ import ca.jrvs.apps.trading.dao.QuoteDao;
 import ca.jrvs.apps.trading.dao.TraderDao;
 import ca.jrvs.apps.trading.model.domain.Account;
 import ca.jrvs.apps.trading.model.domain.PortfolioView;
+import ca.jrvs.apps.trading.model.domain.Position;
+import ca.jrvs.apps.trading.model.domain.Quote;
+import ca.jrvs.apps.trading.model.domain.SecurityRow;
 import ca.jrvs.apps.trading.model.domain.Trader;
 import ca.jrvs.apps.trading.model.domain.TraderAccountView;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataRetrievalFailureException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,7 +53,27 @@ public class DashboardService {
   }
 
   public PortfolioView getProfileViewByTraderId(Integer traderId) {
-    return null;
+
+    List<Position> positionList = positionDao.findAllById(Collections.singletonList(traderId));
+    List<SecurityRow> securityRowList = new ArrayList<>();
+
+    positionList.forEach((position -> {
+      SecurityRow securityRow = new SecurityRow();
+      securityRow.setPosition(position);
+      securityRow.setTicker(position.getTicker());
+
+      Quote quote = quoteDao.findById(position.getTicker()).orElseThrow(() ->
+          new DataRetrievalFailureException("Error finding Quote with symbol/ticker: "
+              + position.getTicker()));
+      securityRow.setQuote(quote);
+
+      securityRowList.add(securityRow);
+    }));
+
+    PortfolioView portfolioView = new PortfolioView();
+    portfolioView.setSecurityRows(securityRowList);
+
+    return portfolioView;
   }
 
   private Account findAccountByTraderId(Integer traderId) {
