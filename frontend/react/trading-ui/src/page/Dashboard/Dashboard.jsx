f1582bd098bb457538c1bc37b31096d7c96c9f36
Chi Calvin Nguyen <chicalvin.nguyen1@gmail.com>
2023-05-03 21:33:42 +0000

    dashboard and navbar

---
+++ frontend/react/trading-ui/src/page/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Dashboard.scss'
+import NavBar from "../../component/NavBar/NavBar";
+
+function Dashboard(props) {
+  return (
+      <div className="dashboard">
+        <NavBar />
+      </div>
+  )
+}
+
+export default Dashboard
